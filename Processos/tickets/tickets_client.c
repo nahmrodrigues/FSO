b6d4ead8c1551cbd19f8caaa2b15f7192d857e49
Natália Rodrigues <nataliamrq@gmail.com>
2019-04-28 23:58:15 -0300

    Finishing tickets implementation.

--- Processos/tickets/tickets_client.c
+++ Processos/tickets/tickets_client.c
@@ -1,3 +1,10 @@
+/*
+    Aluna: Natalia Maria Rodrigues Queiroz
+    Matricula: 16/0015839
+
+    CLIENTE
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,25 +12,29 @@
 #include <mqueue.h>
 #include <sys/stat.h>
 
-#define QUEUE_SERV "/qq"
+#define QUEUE_SERV "/queue_serv"
 #define GET 0
 #define RETURN 1
 
 int client_id;
-char *filename;
-char *client_queue;
+char *filename; // nome do arquivo onde serao colocados 
+                // as informacoes da mensagem enviada ao servidor
+char *client_queue; // nome da queue do client, onde o servidor
+                    // armazenara os tickets enviados aos clients
 
 void set_client_queue() {
     filename = malloc(sizeof(char) * 80);
-    sprintf(filename, "%i", client_id);
+    sprintf(filename, "%i", client_id); // setando o arquivo de informacoes com o id do client
 
     int length = strlen(filename);
 
+    // setando o nome da queue do client com '/id'
     client_queue = malloc(sizeof(char) * (length + 1));
     strcpy(client_queue, "/");
     strcat(client_queue, filename);
 }
 
+// funcao responsavel por atribuir um id para cada client rodado
 void register_client() {
     FILE *fp = fopen("clients.txt", "a+");
 
@@ -39,6 +50,10 @@ void register_client() {
     set_client_queue();
 }
 
+// funcao responsavel por preencher o arquivo de informacoes das msgs enviadas ao servidor
+// control: (GET ou RETURN) indica a solicitacao do client ao servidor
+// ticket: se a solicitacao for do tipo GET, ticket = 0
+//         se a solicitacao for do tipo RETURN, ticket = ticket a ser devolvido 
 void set_msg(int control, int ticket) {
     FILE *fp = fopen(filename, "w+");
     fprintf(fp, "%d\n", control);
@@ -46,30 +61,15 @@ void set_msg(int control, int ticket) {
     fclose(fp);
 }
 
-int c_get_ticket() {     
-    mqd_t queue_serv = mq_open(QUEUE_SERV, O_RDWR);
-    
-    if(queue_serv < 0) {
-        perror("\nget_ticket|serv: mq_open\n");
-        exit(1);
-    }
-    
-    set_msg(GET, 0);
-
-    if((mq_send(queue_serv, (void *) &client_id, sizeof(client_id), 0)) < 0) {
-        perror("\nget_ticket: mq_send\n");
-        exit(1);
-    }
-
-    printf("\nTicket solicitado.\n");
-
+int get_ticket() {
     int ticket;
     struct mq_attr attr;
 
-    attr.mq_maxmsg = 100; // capacidade para 100 mensagens
+    attr.mq_maxmsg = 10; // capacidade para 10 mensagens
     attr.mq_msgsize = sizeof(ticket) ; // tamanho de cada mensagem
     attr.mq_flags = 0;
 
+    // abre a queue do client, onde o servidor ira retornar o ticket
     mqd_t queue_client = mq_open(client_queue, O_RDWR|O_CREAT, 0666, &attr);
 
     if(queue_client < 0) {
@@ -77,13 +77,33 @@ int c_get_ticket() {
         exit(1);
     }
 
+    // abre a fila do servidor, onde o client ira sinalizar uma solicitacao
+    // enviando seu id    
+    mqd_t queue_serv = mq_open(QUEUE_SERV, O_RDWR);
+    
+    if(queue_serv < 0) {
+        perror("\nget_ticket|serv: mq_open");
+        exit(1);
+    }
+
+    set_msg(GET, 0); // preenche o arquivo de informacoes da msg
+
+    // envia uma msg com o id para o servidor
+    if((mq_send(queue_serv, (void *) &client_id, sizeof(client_id), 0)) < 0) {
+        perror("\nget_ticket: mq_send");
+        exit(1);
+    }
+
+    printf("\nTicket solicitado.\n");
+
+    // espera o ticket do servidor na fila do client
     if((mq_receive(queue_client, (void*) &ticket, sizeof(ticket), 0)) < 0) {
         perror("\nget_ticket: mq_receive\n");
         exit(1);
     }
 
     printf("Ticket recebido: %d\n", ticket);
-    remove(filename);
+    remove(filename); // apaga o arquivo de informacoes da msg
     
     mq_close(queue_serv);
     mq_close(queue_client);
@@ -92,15 +112,16 @@ int c_get_ticket() {
 }
 
 void return_ticket(int ticket) {
-    mqd_t queue_serv = mq_open(QUEUE_SERV, O_RDWR);
+    mqd_t queue_serv = mq_open(QUEUE_SERV, O_RDWR); // abre a fila do servidor
 
     if(queue_serv < 0) {
         perror("\nreturn_ticket|serv: mq_open\n");
         exit(1);
     }
 
-    set_msg(RETURN, ticket);
+    set_msg(RETURN, ticket); // preenche o arquivo de informacoes da msg
 
+    // envia msg na fila do servidor, indicando solicitacao
     if((mq_send(queue_serv, (void *) &client_id, sizeof(client_id), 0)) < 0) {
         perror("\nget_ticket: mq_send\n");
         exit(1);
@@ -109,13 +130,13 @@ void return_ticket(int ticket) {
     mq_close(queue_serv);
 }
 
-int main (int argc, char *argv[]) {
-    register_client();
+int main () {
+    register_client(); // registra o novo client, atribuindo um id
 
     int ticket = 0;
 
     while(1) {
-        ticket = c_get_ticket();
+        ticket = get_ticket();
         sleep(1);
         return_ticket(ticket);
     }
--- Processos/tickets/tickets_serv.c
+++ Processos/tickets/tickets_serv.c
@@ -1,3 +1,10 @@
+/*
+    Aluna: Natalia Maria Rodrigues Queiroz
+    Matricula: 16/0015839
+
+    SERVIDOR
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -6,13 +13,14 @@
 #include <mqueue.h>
 #include <unistd.h>
 
-#define QUEUE "/qq"
+#define QUEUE "/queue_serv"
 #define NUM_MAX 100
 #define GET 0
 #define RETURN 1
 
 int tickets[NUM_MAX];
 
+// preenche o vetor de tickets
 void gera_tickets() {
     int i;
 
@@ -21,6 +29,8 @@ void gera_tickets() {
     }
 }
 
+
+// retorna o primeiro valor diferente de 0 no vetor de tickets
 int get_ticket() {
     int i;
 
@@ -29,7 +39,7 @@ int get_ticket() {
     for(i = 0; i < NUM_MAX; i++) {
         if(tickets[i] != 0) {
             result = tickets[i];
-            tickets[i] = 0;
+            tickets[i] = 0; // seta o valor para 0, indicando que o ticket esta sendo usado
             return result;
         }
     }
@@ -37,12 +47,13 @@ int get_ticket() {
     return result;
 }
 
+// coloca o ticket devolvido na posicao correta
 void return_ticket(int t) {
     tickets[t-1] = t;
 }
 
 void handle_get_ticket(int client_id, char *client_queue) {
-    mqd_t queue = mq_open(client_queue, O_RDWR);
+    mqd_t queue = mq_open(client_queue, O_RDWR); // abre a fila do client que enviou a solicitacao
 
     if(queue < 0) {
         perror("\nget: mq_open\n");
@@ -51,6 +62,7 @@ void handle_get_ticket(int client_id, char *client_queue) {
 
     int ticket = get_ticket();
 
+    // envia uma msg na fila do client, com o ticket solicitado
     if((mq_send(queue, (void *) &ticket, sizeof(ticket), 0)) < 0) {
         perror("\nget_ticket: mq_send\n");
         exit(1);
@@ -61,6 +73,8 @@ void handle_get_ticket(int client_id, char *client_queue) {
     mq_close(queue);
 }
 
+// funcao responsavel por retornar o nome do arquivo de informacoes do client especifico
+// o nome do arquivo e sempre o id do client
 char * get_filename(int client_id) {
     char *line = malloc(sizeof(char) * 80);
     sprintf(line, "%i", client_id);
@@ -68,6 +82,7 @@ char * get_filename(int client_id) {
     return line;
 }
 
+// funcao responsavel por retornar o nome da fila do client, que e sempre na forma '/id'
 char * get_client_queue(char *line) {
     int length = strlen(line);
 
@@ -82,6 +97,7 @@ void handle_msg(int client_id) {
     char *filename = get_filename(client_id);
     char *client_queue = get_client_queue(filename);
 
+    // le arquivo de informacoes do client
     FILE *fp = fopen(filename, "r");
     int control;
     int ticket;
@@ -90,10 +106,10 @@ void handle_msg(int client_id) {
     fscanf(fp, "%d", &ticket);
     fclose(fp);
 
-    if(control == GET) {
+    if(control == GET) { // se requisicao for do tipo GET
         handle_get_ticket(client_id, client_queue);
     }
-    else {
+    else { // se requisicao for do tipo RETURN
         return_ticket(ticket);
         printf("\nTicket recebido do cliente %d: %d\n", client_id, ticket);
     }
@@ -101,7 +117,6 @@ void handle_msg(int client_id) {
 
 
 int main() {
-    // mq_unlink(QUEUE);
     gera_tickets();
 
     struct mq_attr attr;
@@ -111,16 +126,15 @@ int main() {
     attr.mq_msgsize = sizeof(client_id); // tamanho de cada mensagem
     attr.mq_flags = 0;
 
-    mqd_t queue = mq_open(QUEUE, O_RDWR|O_CREAT, 0666, &attr);
+    mqd_t queue = mq_open(QUEUE, O_RDWR|O_CREAT, 0666, &attr); // abre/cria fila do servidor
 
     if(queue < 0) {
         perror("mq_open\n");
         exit(1);
     }
 
-    int ticket;
-
     while(1) {
+        // espera msg de requisicao dos clients
         if((mq_receive(queue, (void *) &client_id, sizeof(client_id), 0)) < 0) {
             perror("\nmq_receive");
             exit(1);
